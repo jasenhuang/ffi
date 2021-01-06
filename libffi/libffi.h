d6096618d35b8a9f8d1ba638c0154d992c9ad769
jasenhuang <jasenhuang@tencent.com>
2021-01-06 17:31:32 +0800

    Initial Commit

---
+++ libffi/libffi.h
@@ -0,0 +1,18 @@
+//
+//  libffi.h
+//  libffi
+//
+//  Created by jasenhuang on 2021/1/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libffi.
+FOUNDATION_EXPORT double libffiVersionNumber;
+
+//! Project version string for libffi.
+FOUNDATION_EXPORT const unsigned char libffiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libffi/PublicHeader.h>
+
+
